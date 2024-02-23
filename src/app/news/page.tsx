da3c1a842b26c3cc65e8b73a80431ea9faed71fe
rbarisozkal <barisozkal11@gmail.com>
2024-02-23 23:41:15 +0300

    searching with phrase

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -5,20 +5,70 @@ import { CiSearch } from "react-icons/ci";
 import "./styles.scss";
 export default function News() {
   const [news, setNews] = useState([]);
+  const [phrase, setPhrase] = useState("");
   const topics = [
-    { value: "art", text: "Arts/Music" },
+    { value: "Science", text: "Science" },
     { value: "sports", text: "Sports" },
-    { value: "politics", text: "Politics" },
-    { value: "culture", text: "Culture" },
+    { value: "health", text: "Health" },
+    { value: "entertainment", text: "Entertainment" },
+    { value: "general", text: "General" },
+    { value: "technology", text: "Technology" },
+    { value: "business", text: "Business" },
   ];
-  const getNews = (topic: any) => {
-    console.log(topic);
+  const getNewsByPhrase = async (phrase: string) => {
+    //https://newsapi.org/v2/everything?q=Apple&from=2024-02-23&sortBy=popularity&apiKey=API_KEY
+    const today = new Date();
+    const oneWeekAgo = new Date(today);
+    oneWeekAgo.setDate(today.getDate() - 7);
+
+    const year = oneWeekAgo.getFullYear();
+    const month = oneWeekAgo.getMonth() + 1; // Months are zero-indexed
+    const day = oneWeekAgo.getDate();
+    const formattedDate = `${year}-${month < 10 ? "0" + month : month}-${
+      day < 10 ? "0" + day : day
+    }`;
+
+    try {
+      let response = await fetch(
+        `https://newsapi.org/v2/everything?q=${phrase}&from=${formattedDate}&sortBy=popularity&pageSize=20
+        &apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+      );
+
+      if (response.ok) {
+        let phraseRelatedArticles = await response.json();
+        console.log(phraseRelatedArticles);
+        setNews(phraseRelatedArticles.articles);
+      } else {
+        console.error(
+          "Failed to fetch news:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    }
   };
-  const getAllNews = async () => {
-    let response = fetch(
-      `https://newsapi.org/v2/everything?apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
-    );
-    console.log((await response).json());
+  const getNews = async (topic: any) => {
+    try {
+      let response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=tr&category=${topic}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+      );
+
+      if (response.ok) {
+        let topicRelatedArticles = await response.json();
+        console.log(topicRelatedArticles);
+        setNews(topicRelatedArticles.articles);
+      } else {
+        console.error(
+          "Failed to fetch news:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    }
   };
   useEffect(() => {
     //getAllNews();
@@ -31,14 +81,24 @@ export default function News() {
           name="price"
           id="price"
           className="block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  my-4 px-4"
-          placeholder="Search for topics..."
+          placeholder="Search for news from last week..."
+          onChange={(e) => setPhrase(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              getNewsByPhrase(e.target.value);
+              e.target.value = "";
+            }
+          }}
         />
-        <div>
+        <div className="flex flex-wrap">
           {" "}
           {topics.map((topic) => {
-            console.log(topic);
             return (
-              <span onClick={() => getNews(topic.value)} className="topic">
+              <span
+                key={topic.value}
+                onClick={() => getNews(topic.value)}
+                className="topic"
+              >
                 {topic.text}
               </span>
             );
